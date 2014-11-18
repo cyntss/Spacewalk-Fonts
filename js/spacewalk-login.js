98f311206a3bf8c7ce0eab7625f0d80080374077
Cynthia Sanchez <csanchez@suse.com>
2014-11-18 17:32:32 +0100

    branch cleaned up

--- js/spacewalk-login.js
+++
@@ -1,9 +0,0 @@
-$(document).on("ready", function(){
-	$("aside").remove();
-	$(document).ready(function() {
-	    var me = $("section");
-	    var newMe = $("<div class='login-page'>");
-	    newMe.html(me.html());
-	    me.replaceWith(newMe);
-	});
-});
